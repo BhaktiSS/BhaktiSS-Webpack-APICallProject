e9eafaaedbcded8ab99c8d35ca2e6aa3ce2e95d4
BhaktiSS <bhakti589@gmail.com>
2021-06-13 20:01:49 -0700

    first commit

---
+++ src/server/index.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+dotenv.config();
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+
+const app = express()
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+const APIKey= process.env.API_KEY
+var textapi = new MeaningCloud ({
+   application_key: APIKey
+});
+
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
